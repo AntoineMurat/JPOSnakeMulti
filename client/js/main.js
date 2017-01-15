61b1d7ef6c72fe0620bdd12580bd7cbf2ec29bb4
Evoliofly <antoine.murat@protonmail.com>
2017-01-15 13:04:50 +0100

    Menu mobile activé.

--- client/js/main.js
+++ client/js/main.js
@@ -2,18 +2,54 @@ Number.prototype.mod = function(n) {
 	return ((this % n) + n) % n;
 };
 
-var socket = io('http://localhost');
-
+/**
+ * Lorsque la DOM sera chargée :
+ */
 $(function() {
 
+	/**
+	 * Materialize
+	 *
+	 * On active le menu.
+	 */
+
+	$(".button-collapse").sideNav();
+
+	/**
+	 * On pipe la sortie de la console dans l'HTML.
+	 */
 	ConsoleLogHTML.connect($("#console"));
 
+	/**
+	 * Lorsqu'on aura récupéré notre ID, on lancera la partie.
+	 *
+	 * C'est pourquoi on attend d'être connecté.
+	 */
 	socket.on('connect', function () {
 
-		new Game(
+		/**
+		 * On instane une nouvelle partie.
+		 */
+		var game = new Game(
 			$("canvas")[0].getContext("2d")
-		).start()
+		)
+
+		/**
+		 * On la démarre.
+		 */
+		game.start()
+
+		/**
+		 * Alias.
+		 *
+		 * On définit un ensemble d'alias pour aider le dev' débutant.
+		 */
 
 	})
 
 })
+
+/**
+ * On établit la connexion avec le serveur.
+ */
+var socket = io('http://localhost');
